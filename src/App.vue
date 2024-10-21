521125239b5fb76b285160b0de06878ebcbbcba4
analiarojas2900 <analia.rojas@duocuc.cl>
2024-10-21 09:31:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const run = ref('22.222.222-2')
+const apellidos = ref('MUSK')
+const nombres = ref('ELON REEVE')
+const nacionalidad = ref('SUDAFRICANO')
+const sexo = ref('M')
+const fechaNacimiento = ref('28 JUN 1971')
+const numeroDocumento = ref('222.222.222')
+const fechaEmision = ref('01 ENE 2025')
+const fechaVencimiento = ref('01 ENE 2030')
+const firmaTitular = ref('X Æ A-12')
+</script>
